fd6c805f7c73987d616b2d203080d4a7a82f19e9
adamgiovanelli <adamgiovanelli@gmail.com>
2023-01-19 07:53:45 -0500

    added playGame function to play rounds for a  best 3 out of 5 game

--- rps.js
+++ rps.js
@@ -17,55 +17,92 @@ function getComputerChoice(){
 }
 
 function getPlayerChoice(){
-    let playerMove = prompt("Enter your move: Rock, Paper, or Scissors")
+    let validChoice = 0;
+
+    while(!validChoice){
+        let playerMove = prompt("Enter your move: Rock, Paper, or Scissors")
     
-    playerMoveLowerCase = playerMove.toLowerCase();
-    nonCapitalLetters = playerMoveLowerCase.slice(1);
-    firstLetterCapital = playerMoveLowerCase[0].toUpperCase();
-    finalPlayerMove = firstLetterCapital+nonCapitalLetters;
+        //format answer to make first letter capital and rest lowercase
+        let playerMoveLowerCase = playerMove.toLowerCase();
+        let nonCapitalLetters = playerMoveLowerCase.slice(1);
+        let firstLetterCapital = playerMoveLowerCase[0].toUpperCase();
+        let finalPlayerMove = firstLetterCapital+nonCapitalLetters;
 
-    if (finalPlayerMove != 'Rock' || finalPlayerMove != 'Paper' || finalPlayerMove != 'Scissors')
-
-    //add if not rock paper or scissors, please enter valid move
-    //keep prompting until valid
-    return finalPlayerMove;
+        //check if valid move was entered
+        if (finalPlayerMove == 'Rock' || finalPlayerMove == 'Paper' || finalPlayerMove == 'Scissors'){
+            validChoice = 1;
+            return finalPlayerMove;
+        }
+    }   
 }
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
         console.log("It's a tie!")
+        return "Tie";
     }
     else if (playerSelection == 'Rock'){
         if (computerSelection == 'Scissors'){
             console.log("You win!")
+            return "Player";
         }
         else{
             console.log("Computer wins!")
+            return "Computer";
         }
     }
     else if (playerSelection == 'Paper'){
         if (computerSelection == 'Rock'){
             console.log("You win!")
+            return "Player";
         }
         else{
             console.log("Computer wins!")
+            return "Computer";
         }
     }
     else{
         if (computerSelection == 'Paper'){
             console.log("You win!")
+            return "Player";
         }
         else{
             console.log("Computer wins!")
+            return "Computer";
         }
     } 
 }
 
-const playerSelection = getPlayerChoice();
-console.log(playerSelection);
+function playGame(){
+    let playerWins = 0;
+    let computerWins = 0;
 
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
+    while(playerWins < 3 && computerWins < 3){
+        const playerSelection = getPlayerChoice();
+        console.log(playerSelection);
 
-playRound(playerSelection, computerSelection);
+        const computerSelection = getComputerChoice();
+        console.log(computerSelection);
+
+        let roundWinner = playRound(playerSelection, computerSelection);
+
+        if (roundWinner == 'Player'){
+            playerWins++
+        }
+        //need else if, not just else, because of ties
+        else if (roundWinner == 'Computer'){
+            computerWins++
+        }
+    }
+
+    if (playerWins == 3){
+        console.log("You won the game!")
+    }
+    else{
+        console.log("You lost the game.")
+    }
+}
+
+playGame();
+
 
